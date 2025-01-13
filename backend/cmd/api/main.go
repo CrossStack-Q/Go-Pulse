14967a2ad28ab77b6e67a4c3e112b96aded15f47
crossstack-q <crossstack@gmail.com>
2025-01-13 21:41:08 +0530

    Posts: &PostStore{db},

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -4,14 +4,19 @@ import (
 	"log"
 
 	"github.com/CrossStack-Q/Go-Pulse/backend/internal/env"
+	"github.com/CrossStack-Q/Go-Pulse/backend/internal/store"
 )
 
 func main() {
 	cfg := config{
 		addr: env.GetString("ADDR", ":8900"),
 	}
+
+	store := store.NewStorage(nil)
+
 	app := &application{
 		config: cfg,
+		store:  store,
 	}
 
 	log.Fatal(app.run(app.mount()))
