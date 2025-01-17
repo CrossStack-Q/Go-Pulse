f854154d2d6e8f69fb618cc22653127420ec2df1
crossstack-q <crossstack@gmail.com>
2025-01-17 13:44:26 +0530

    DB Conn Done

--- backend/cmd/api/api.go
+++ backend/cmd/api/api.go
@@ -17,6 +17,14 @@ type application struct {
 
 type config struct {
 	addr string
+	db   dbConfig
+}
+
+type dbConfig struct {
+	addr         string
+	maxopenConns int64
+	maxIdleConn  int64
+	maxIdleTime  string
 }
 
 func (app *application) mount() http.Handler {
--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/CrossStack-Q/Go-Pulse/backend/internal/db"
 	"github.com/CrossStack-Q/Go-Pulse/backend/internal/env"
 	"github.com/CrossStack-Q/Go-Pulse/backend/internal/store"
 )
@@ -10,9 +11,29 @@ import (
 func main() {
 	cfg := config{
 		addr: env.GetString("ADDR", ":8900"),
+		db: dbConfig{
+			addr:         env.GetString("DB_ADDR", "postgres://admin:dsa@localhost:5433/titi?sslmode=disable"),
+			maxopenConns: int64(env.GetInt("Max_Conn", 30)),
+			maxIdleConn:  int64(env.GetInt("Max_Idle_Conn", 30)),
+			maxIdleTime:  "3m",
+		},
 	}
 
-	store := store.NewStorage(nil)
+	db, err := db.New(
+		cfg.db.addr,
+		int(cfg.db.maxopenConns),
+		int(cfg.db.maxopenConns),
+		cfg.db.maxIdleTime,
+	)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	defer db.Close()
+	log.Println("database connection pool established")
+
+	store := store.NewStorage(db)
 
 	app := &application{
 		config: cfg,
